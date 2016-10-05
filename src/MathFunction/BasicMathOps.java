c7db869f887471162bcdea02960d3093abd20e6f
Raúl Torrijos <raultorr@ucm.es>
2016-10-05 13:24:05 +0200

    BasicMathOps added

---
+++ src/MathFunction/BasicMathOps.java
@@ -0,0 +1,12 @@
+package MathFunction;
+
+public class BasicMathOps {
+
+	public static int sum (int a, int b){
+		return a+b;
+	}
+	
+	public static int product (int a, int b){
+		return a*b;
+	}
+}
